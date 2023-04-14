6528876ff910e8765d2f812f9125d6df69112f53
SusanOnuorah <69466968+SusanOnuorah@users.noreply.github.com>
2023-04-13 19:43:17 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.83.178.200:7000/',
+  serverUrl: 'http://3.99.144.10:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
